294892cd50bd1676ace6602e82fcca7cb9032d1a
Zkyarpan <karkiarpan555@gmail.com>
2024-06-23 07:22:02 +0545

    add error handling

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,37 +1,50 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import bcrypt from "bcrypt";
-
 import userModel from "./userModel";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
+
   // Validation
   if (!name || !email || !password) {
-    const error = createHttpError(400, "All feilds are required.");
+    const error = createHttpError(400, "All fields are required.");
     return next(error);
   }
-  // Database Call
-  const user = await userModel.findOne({ email });
-  if (user) {
-    const error = createHttpError(400, "User already exists with this email!");
-    return next(error);
+
+  try {
+    // Check if user already exists
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(
+        400,
+        "User already exists with this email!"
+      );
+      return next(error);
+    }
+
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    const newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Generate JWT token
+    const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+    });
+
+    // Response
+    res.json({ accessToken: token });
+  } catch (error) {
+    next(createHttpError(500, "Error while creating new user."));
   }
-  //   Password -> Hash
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const newUser = await userModel.create({
-    name,
-    email,
-    password : hashedPassword,
-  });
-  // Token Generation JWT token
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-  });
-  // Response
-  res.json({ accessToken: token });
 };
 
 export { createUser };
