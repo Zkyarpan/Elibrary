a099995b84b71f6396d1745f6062b11ed695aa3a
Zkyarpan <karkiarpan555@gmail.com>
2024-06-30 22:29:54 +0545

    add images in cloudinary cloud

--- src/Book/bookController.ts
+++ src/Book/bookController.ts
@@ -1,9 +1,25 @@
 import { Request, Response, NextFunction } from "express";
+import cloudinary from "../config/cloudinary";
+import path from "node:path";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({
-    
+  console.log("Files", req.files);
+
+  const files = req.files as { [filename: string]: Express.Multer.File[] };
+  const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+  const fileName = files.coverImage[0].filename;
+  const filePath =  path.resolve(
+    __dirname,
+    "../../public/data/uploads",
+    fileName
+  );
+
+  const uploadResult = await cloudinary.uploader.upload(filePath, {
+    filename_override: fileName,
+    folder: "book-covers",
+    format: coverImageMimeType,
   });
+  res.json({});
 };
 
-export  { createBook };
+export { createBook };
