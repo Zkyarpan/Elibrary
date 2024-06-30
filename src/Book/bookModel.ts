af8173edcccefd2dac5355854b55485e4696b26c
Zkyarpan <karkiarpan555@gmail.com>
2024-06-30 21:17:14 +0545

    add book model

---
+++ src/Book/bookModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { mongo } from "mongoose";
+import { Book } from "./booksTypes";
+
+const bookSchema = new mongoose.Schema<Book>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+      required: true,
+    },
+    file: {
+      type: String,
+      required: true,
+    },
+    genre: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model<Book>("Book", bookSchema);
